bb695199662ce86ff963f845deeb563cf80e66fd
mlatifr <mlatifraldi@gmail.com>
2021-11-11 03:42:36 +0700

    akuntanPageInputPenjurnalan:MbetulinKrjgPenjurnaln

---
+++ lib/akuntan/page_input_penjurnalan/akuntan_get_daftar_akun.dart
@@ -0,0 +1,38 @@
+library flutter_application_1.kasir_get_antrean;
+
+import 'dart:async';
+import 'dart:convert';
+import 'package:flutter_application_1/main.dart';
+import 'package:http/http.dart' as http;
+
+List<AkuntanVDftrAkun> AkntVDftrAkns = [];
+List<String> DebetKredit = ['debet', 'kredit'];
+var valIdAkun, valueNamaAkun, valueDebetKredit;
+
+class AkuntanVDftrAkun {
+  var idAkun, namaAkun;
+  AkuntanVDftrAkun({
+    this.idAkun,
+    this.namaAkun,
+  });
+
+  // untuk convert dari jSon
+  factory AkuntanVDftrAkun.fromJson(Map<String, dynamic> json) {
+    return new AkuntanVDftrAkun(
+      idAkun: json['id'],
+      namaAkun: json['nama'],
+    );
+  }
+}
+
+Future<String> fetchDataAkuntanVDftrAkun() async {
+  final response = await http.post(
+    Uri.parse(APIurl + "akuntan_v_dftr_akun.php"),
+  );
+  if (response.statusCode == 200) {
+    // print(response.body);
+    return response.body;
+  } else {
+    throw Exception('Failed to read API');
+  }
+}
