96b77adc3fd9772660921388e9c27448dd2304c9
mlatifr <mlatifraldi@.com>
2022-07-01 13:49:08 +0700

    swicth status pegawai aktif non-aktif

---
+++ lib/pemilik/tambah_pegawai/view/add_pegawai_view.dart
@@ -0,0 +1,224 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/pemilik/tambah_pegawai/service/post_add_pegawai.dart';
+
+Future<void> AddPegawaiModalBottom(context, _pegawaiController) {
+  TextEditingController _username = TextEditingController();
+  TextEditingController _sandi = TextEditingController();
+  TextEditingController _sandi2 = TextEditingController();
+  TextEditingController _nama = TextEditingController();
+  TextEditingController _tlp = TextEditingController();
+  TextEditingController _alamat = TextEditingController();
+  Icon ikonKonfirmasiPassword = Icon(
+    Icons.remove_circle,
+    color: Colors.red,
+  );
+  Icon ikonViciblityPassword = Icon(
+    Icons.visibility_off,
+    // color: Colors.red,
+  );
+  bool boolVisibility = true;
+  return showModalBottomSheet<void>(
+      context: context,
+      builder: (context) {
+        return StatefulBuilder(builder: (BuildContext context,
+            StateSetter setState /*You can rename this!*/) {
+          return SafeArea(
+            child: Container(
+              padding: EdgeInsets.all(20),
+              height: MediaQuery.of(context).size.height +
+                  (MediaQuery.of(context).size.height * 0.2),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  TextFormField(
+                    controller: _username,
+                    decoration: InputDecoration(
+                      hintText: 'Username',
+                    ),
+                  ),
+                  Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Expanded(
+                        child: TextFormField(
+                            obscureText: boolVisibility,
+                            enableSuggestions: false,
+                            autocorrect: false,
+                            controller: _sandi,
+                            decoration: InputDecoration(
+                              hintText: 'Password',
+                            )),
+                      ),
+                      IconButton(
+                        icon: ikonViciblityPassword,
+                        onPressed: () {
+                          setState(() {
+                            boolVisibility = !boolVisibility;
+                            if (boolVisibility) {
+                              ikonViciblityPassword = Icon(Icons.visibility);
+                            }
+                            if (!boolVisibility) {
+                              ikonViciblityPassword =
+                                  Icon(Icons.visibility_off);
+                            }
+                          });
+                        },
+                      )
+                    ],
+                  ),
+                  Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Expanded(
+                        child: TextFormField(
+                          obscureText: boolVisibility,
+                          enableSuggestions: false,
+                          autocorrect: false,
+                          controller: _sandi2,
+                          decoration: InputDecoration(
+                            hintText: 'Konfirmasi password',
+                          ),
+                          onChanged: (value) {
+                            print('value: $value');
+                            if (_sandi.text == value.toString() &&
+                                value != '') {
+                              setState(() {
+                                ikonKonfirmasiPassword = Icon(
+                                  Icons.check_circle,
+                                  color: Colors.green,
+                                );
+                              });
+                            } else {
+                              setState(() {
+                                ikonKonfirmasiPassword = Icon(
+                                  Icons.remove_circle,
+                                  color: Colors.red,
+                                );
+                              });
+                            }
+                          },
+                        ),
+                      ),
+                      ikonKonfirmasiPassword
+                    ],
+                  ),
+                  TextFormField(
+                    controller: _nama,
+                    decoration: InputDecoration(
+                      hintText: 'Nama Akun',
+                    ),
+                  ),
+                  TextFormField(
+                    controller: _tlp,
+                    decoration: InputDecoration(
+                      hintText: 'Telepon',
+                    ),
+                  ),
+                  TextFormField(
+                    controller: _alamat,
+                    maxLines: 5,
+                    minLines: 2,
+                    decoration: InputDecoration(
+                      hintText: 'Alamat',
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            primary: (Colors.red[400])),
+                        child: const Text(
+                          'Batal',
+                          style: TextStyle(color: Colors.black),
+                        ),
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                      ),
+                      SizedBox(width: 22),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            primary: (Colors.blue[400])),
+                        child: const Text(
+                          'Simpan',
+                          style: TextStyle(color: Colors.black),
+                        ),
+                        onPressed: () {
+                          modalBottomKonfirmasi(context, _pegawaiController,
+                              _username, _sandi, _nama, _tlp, _alamat);
+                        },
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          );
+        });
+      });
+}
+
+Future modalBottomKonfirmasi(BuildContext context, _pegawaiController,
+    _username, _sandi, _namaAkun, _tlp, _alamat) {
+  return showModalBottomSheet(
+    context: context,
+    builder: (context) {
+      return Container(
+        padding: EdgeInsets.all(20),
+        height: MediaQuery.of(context).size.height,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Simpan:'
+                '\nUsername: \n${_username.text}'
+                '\nNama Akun:\n${_namaAkun.text}'
+                '\nTelepon: \n${_tlp.text}'
+                '\nAlamat:\n${_alamat.text}'),
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(primary: (Colors.red[400])),
+                  child: const Text(
+                    'Batal',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                  onPressed: () {
+                    Navigator.pop(context);
+                    // Get.back();
+                  },
+                ),
+                SizedBox(width: 22),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(primary: (Colors.blue[400])),
+                  child: const Text(
+                    'Simpan',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                  onPressed: () {
+                    PostAddPegawai(_username.text, _sandi.text, _namaAkun.text,
+                            _tlp.text, _alamat.text)
+                        .then((value) {
+                      print(value);
+                      if (value.toString().contains('success')) {
+                        Navigator.pop(context);
+                        Navigator.pop(context);
+                      }
+                    });
+                  },
+                ),
+              ],
+            ),
+          ],
+        ),
+      );
+    },
+  );
+}
--- lib/pemilik/tambah_pegawai/view/daftar_pegawai_view.dart
+++ lib/pemilik/tambah_pegawai/view/daftar_pegawai_view.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_1/pemilik/tambah_pegawai/controller/daftar_pegawai_controller.dart';
 import 'package:flutter_application_1/pemilik/tambah_pegawai/service/get_info_pegawai.dart';
 
-import 'modalBottomAddPegawai.dart';
+import 'add_pegawai_view.dart';
 
 class DaftarPegawai extends StatefulWidget {
   @override
@@ -11,6 +11,7 @@ class DaftarPegawai extends StatefulWidget {
 
 class _DaftarPegawaiState extends State<DaftarPegawai> {
   DaftarPegawaiController _pegawaiController = DaftarPegawaiController();
+  List listSatusButton = [];
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -40,51 +41,97 @@ class _DaftarPegawaiState extends State<DaftarPegawai> {
                       if (snapshot.connectionState == ConnectionState.done) {
                         // print((snapshot.data.runtimeType.toString()));
                         _pegawaiController.ConvertJsonListPegawai(snapshot);
-                        return DataTable(
-                            headingRowColor: MaterialStateColor.resolveWith(
-                                (states) => Colors.blue[100]),
-                            columns: [
-                              DataColumn(label: Text('Nama')),
-                              DataColumn(label: Text('Telepon')),
-                              DataColumn(label: Text('Status')),
-                              DataColumn(label: Text('Unit\nKerja')),
-                              DataColumn(label: Text('Edit')),
-                            ],
-                            rows: [
-                              for (var i = 0;
-                                  i < _pegawaiController.listPegawai.length;
-                                  i++)
-                                DataRow(
-                                    onLongPress: () {
-                                      print(
-                                          'buka halaman edit ${_pegawaiController.listPegawai[i].nama}');
-                                    },
-                                    cells: [
-                                      DataCell(Text(
-                                          '${_pegawaiController.listPegawai[i].nama}')),
-                                      DataCell(Text(
-                                          '${_pegawaiController.listPegawai[i].tlp}')),
-                                      DataCell(Text(
-                                          '${_pegawaiController.listPegawai[i].status}')),
-                                      DataCell(Text(
-                                          '${_pegawaiController.listPegawai[i].unitKerja}')),
-                                      DataCell(IconButton(
-                                          onPressed: () {},
-                                          icon: Icon(
-                                            Icons.edit,
-                                            color: Colors.blue,
-                                          ))),
-                                      // if (i >= 5)
-                                      //   DataCell(Text(
-                                      //     'Aktif',
-                                      //     style: TextStyle(
-                                      //         backgroundColor: Colors.blue[200]),
-                                      //   )),
-                                      // if (i < 5) DataCell(Text('Non-Aktif')),
-                                      // if (i >= 5) DataCell(Text('Admin')),
-                                      // if (i < 5) DataCell(Text('Perawat')),
-                                    ]),
-                            ]);
+                        for (var i = 0; i < _pegawaiController.listPegawai.length; i++) {
+                          if (_pegawaiController.listPegawai[i].status == 'aktif') {
+                            listSatusButton.add(true);
+                          }
+                          if (_pegawaiController.listPegawai[i].status == 'non-aktif') {
+                            listSatusButton.add(false);
+                          }
+                        }
+                        return DataTable(headingRowColor: MaterialStateColor.resolveWith((states) => Colors.blue[100]), columns: [
+                          DataColumn(label: Text('Nama')),
+                          DataColumn(label: Text('Telepon')),
+                          DataColumn(label: Text('Status')),
+                          DataColumn(label: Text('Unit\nKerja')),
+                          DataColumn(label: Text('Edit')),
+                        ], rows: [
+                          for (var i = 0; i < _pegawaiController.listPegawai.length; i++)
+                            DataRow(
+                                onLongPress: () {
+                                  print('buka halaman edit ${_pegawaiController.listPegawai[i].nama}');
+                                },
+                                cells: [
+                                  DataCell(Text('${_pegawaiController.listPegawai[i].nama}')),
+                                  DataCell(Text('${_pegawaiController.listPegawai[i].tlp}')),
+                                  DataCell(Text('${_pegawaiController.listPegawai[i].status}')),
+                                  DataCell(Text('${_pegawaiController.listPegawai[i].unitKerja}')),
+                                  DataCell(IconButton(
+                                      onPressed: () {
+                                        showModalBottomSheet(
+                                            context: context,
+                                            builder: (context) {
+                                              return StatefulBuilder(builder: (BuildContext context, StateSetter setState) {
+                                                return SafeArea(
+                                                    child: Container(
+                                                        padding: EdgeInsets.all(20),
+                                                        height: MediaQuery.of(context).size.height + (MediaQuery.of(context).size.height * 0.2),
+                                                        child: Column(
+                                                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                                                          children: [
+                                                            Align(alignment: Alignment.centerLeft, child: Text('Nama')),
+                                                            TextFormField(
+                                                              decoration: InputDecoration(
+                                                                hintText: 'Nama',
+                                                              ),
+                                                            ),
+                                                            Align(alignment: Alignment.centerLeft, child: Text('Status')),
+                                                            Row(
+                                                              children: [
+                                                                Switch(
+                                                                    value: listSatusButton[i],
+                                                                    onChanged: (bool value) {
+                                                                      setState((() {
+                                                                        listSatusButton[i] = value;
+                                                                      }));
+                                                                      for (var element in listSatusButton) {
+                                                                        print(element);
+                                                                      }
+                                                                    }),
+                                                              ],
+                                                            ),
+                                                            Align(alignment: Alignment.centerLeft, child: Text('Telpon')),
+                                                            TextFormField(
+                                                              decoration: InputDecoration(
+                                                                hintText: 'Tlp',
+                                                              ),
+                                                            ),
+                                                            Align(alignment: Alignment.centerLeft, child: Text('Unit Kerja')),
+                                                            TextFormField(
+                                                              decoration: InputDecoration(
+                                                                hintText: 'Unit Kerja',
+                                                              ),
+                                                            ),
+                                                          ],
+                                                        )));
+                                              });
+                                            });
+                                      },
+                                      icon: Icon(
+                                        Icons.edit,
+                                        color: Colors.blue,
+                                      ))),
+                                  // if (i >= 5)
+                                  //   DataCell(Text(
+                                  //     'Aktif',
+                                  //     style: TextStyle(
+                                  //         backgroundColor: Colors.blue[200]),
+                                  //   )),
+                                  // if (i < 5) DataCell(Text('Non-Aktif')),
+                                  // if (i >= 5) DataCell(Text('Admin')),
+                                  // if (i < 5) DataCell(Text('Perawat')),
+                                ]),
+                        ]);
                       } else {
                         return Text('data waiting');
                       }
